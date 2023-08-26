7044588b3f4e5fd1b7748b6a49ce582e7f0a1218
lemony452 <gij9808@gmail.com>
2023-08-26 19:05:48 +0900

    :lipstick: 하단 네비게이션 탭 ui 전면 수정

--- src/components/NavigationTap.tsx
+++ src/components/NavigationTap.tsx
@@ -1,5 +1,7 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { IoLocationOutline, IoMapOutline } from 'react-icons/io5';
+import { BsBookmarkFill, BsBookmark } from 'react-icons/bs'
 
 interface TabProps {
   tabNum: number,
@@ -11,37 +13,29 @@ function NavigationTap({ tabNum, setTabNum } : TabProps) {
   const navigate = useNavigate();
   
   return (
-    <ul className="text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow flex dark:divide-gray-700 dark:text-gray-400">
-      <li className="w-full">
-        <div onClick={()=>{
-          navigate('/');
-          setTabNum(0);
-        }}
-          className={"inline-block w-full p-4 " + (tabNum === 0 ? "text-blue-700" : "text-black-700")}>
-            내 지역
+    <div className='bg-white w-full border-t border-gray-200 flex justify-stretch rounded-xl overflow-hidden py-2'>
+      <button onClick={()=> { setTabNum(0); navigate('/') }}
+        className='flex flex-grow items-center justify-center p-2 text-gray-500 hover:text-gray-900'>
+        <div className={(tabNum === 0 ? 'text-gray-900 ' : null) + 'text-center'}>
+          <span className='block h-8 text-4xl leading-8 mb-4'><IoLocationOutline className='mx-auto' /></span>
+          <span className='block text-m leading-none'>내 지역보기</span>
         </div>
-      </li>
-      <li className="w-full">
-        <div onClick={()=>{
-          navigate('/all');
-          setTabNum(1);
-        }} 
-          className={"inline-block w-full p-4 "
-            + (tabNum === 1 ? "text-blue-700" : "text-black-700")}>
-            모든 지역
+      </button>
+      <button onClick={()=> { setTabNum(1); navigate('/all') }}
+        className='flex flex-grow items-center justify-center p-2 text-gray-500 hover:text-gray-900'>
+        <div className={(tabNum === 1 ? 'text-gray-900 ' : null) + 'text-center'}>
+          <span className='block h-8 text-4xl leading-8 mb-4'><IoMapOutline className='mx-auto' /></span>
+          <span className='block text-m leading-none'>모든 시/도 보기</span>
         </div>
-      </li>
-      <li className="w-full">
-        <div onClick={()=>{
-          navigate('/bookmark');
-          setTabNum(2);
-        }}
-          className={"inline-block w-full p-4 "
-            + (tabNum === 2 ? "text-blue-700" : "text-black-700")}>
-            즐겨찾기
+      </button>
+      <button onClick={()=> { setTabNum(2); navigate('/bookmark') }}
+        className='flex flex-grow items-center justify-center p-2 text-gray-500 hover:text-gray-900'>
+        <div className={(tabNum === 2 ? 'text-gray-900 ' : null) + 'text-center'}>
+          <span className='block h-8 text-3xl leading-8 mb-4'><BsBookmark className='mx-auto' /></span>
+          <span className='block text-m leading-none'>즐겨찾기</span>
         </div>
-      </li>
-    </ul>
+      </button>
+    </div>
   )
 }
 
