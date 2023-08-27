72a5736c9a453e4583ba27ba659acd88d996433a
lemony452 <gij9808@gmail.com>
2023-08-27 15:37:47 +0900

    :truck: MyLocation을 SelectLocation으로 이름 변경 후 utils 폴더로 위치 변경

---
+++ src/utils/utils.ts
@@ -0,0 +1,8 @@
+export interface SelctLocationData {
+  stationName: string,
+  dataTime: string,
+  pm10Value: number | null,
+  pm10Grade: number | null,
+  pm25Value: number | null,
+  pm25Grade: number | null,
+}
