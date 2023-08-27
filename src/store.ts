33b51d7153f985108144b6411bc883315fea89d8
lemony452 <gij9808@gmail.com>
2023-08-27 23:49:23 +0900

    :bug: Bookmark state와 로컬 스토리지 연결 및 리듀서 함수 반영
    removeLocation 함수에서 indexOf 함수를 for문 비교로 변경

--- src/store.ts
+++ src/store.ts
@@ -1,29 +1,43 @@
 import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface LocationType {
-  sido: string,
-  station: string,
+export interface LocationType {
+  sidoN: string,
+  stationN: string,
 }
 
-let bookmark = createSlice({
+let bookmarkList = JSON.parse(localStorage.getItem('bookmarkList')!);
+console.log(bookmarkList);
+
+let bookmarkSlice = createSlice({
   name: 'bookmark',
-  initialState: [] as LocationType[],
+  initialState: bookmarkList as LocationType[],
   reducers: {
     addLocation(state, action: PayloadAction<LocationType>) {
+      let temp = [...state];
+      temp.push(action.payload);
+      localStorage.setItem('bookmarkList', JSON.stringify(temp));
       state.push(action.payload);
     },
     removeLocation(state, action: PayloadAction<LocationType>) {
-      let idx = state.indexOf(action.payload);
-      if (idx !== -1) state.splice(idx, 1);
+      let temp = [...state];
+      for (let i=0; i<state.length; i++) {
+        if (state[i].sidoN === action.payload.sidoN && state[i].stationN === action.payload.stationN) {
+          state.splice(i, 1);
+          temp.splice(i, 1);
+        }
+      }
+      localStorage.setItem('bookmarkList', JSON.stringify(temp));
     }
   }
 })
 
 const store = configureStore({
   reducer: {
-    bookmark: bookmark.reducer
+    bookmark: bookmarkSlice.reducer
   }
 })
+
+export const { addLocation, removeLocation } = bookmarkSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>
 export default store
