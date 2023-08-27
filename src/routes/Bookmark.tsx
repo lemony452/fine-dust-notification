224938d503703f5f9caa75042bd5fcbfb47e526f
lemony452 <gij9808@gmail.com>
2023-08-27 23:51:35 +0900

    :memo: Return문 예외처리 코드 구조 변경

--- src/routes/Bookmark.tsx
+++ src/routes/Bookmark.tsx
@@ -3,6 +3,7 @@ import { RootState, removeLocation, LocationType } from "../store";
 import { GetDustData } from "../utils/getDustData";
 import { SelctLocationData } from '../utils/utils';
 import Card from "../components/Card";
+import { useEffect } from "react";
 
 interface DataType extends SelctLocationData {
   sidoName: string
@@ -16,24 +17,27 @@ function Bookmark() {
   const allDustData = GetDustData('전국');
   
   let res: DataType[] = []
+
+  // useEffect(() => {
+  // }, [allDustData.data])
   if (allDustData.data !== null && allDustData.data !== undefined && bookmarkList.length > 0) {
     // console.log(allDustData.data);
     for (let i=0; i<bookmarkList.length; i++) {
       for (let j=0; j<allDustData.data.length; j++) {
-        if (bookmarkList[i].sido === allDustData.data[j].sidoName && bookmarkList[i].station === allDustData.data[j].stationName) res.push(allDustData.data[j])
+        if (bookmarkList[i].sidoN === allDustData.data[j].sidoName && bookmarkList[i].stationN === allDustData.data[j].stationName) res.push(allDustData.data[j])
       }
     }
   }
-
   console.log(res);
+
 
   return (
     <div>
-      { bookmarkList.length === 0 ? '즐겨찾는 지역을 추가해주세요!' : 
-        allDustData.isLoading ? '로딩중' :
-        allDustData.isError ? 'Error!' :
-        res.map((value: DataType) => <Card sido={value.sidoName} dustData={value}></Card>)
-      }
+      { bookmarkList.length === 0 && '즐겨찾는 지역을 추가해주세요!' }
+      { allDustData.isLoading && '로딩중' }
+      { allDustData.isError && 'Error!' }
+      { allDustData && (res.length !== 0) &&
+        res.map((value: DataType, idx: number) => <Card key={idx} sido={value.sidoName} dustData={value}></Card>) }
     </div>
   )
 }
