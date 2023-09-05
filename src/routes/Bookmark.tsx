86bdac1a5a14f169ad42985f25e58332149b7320
lemony452 <gij9808@gmail.com>
2023-09-06 00:20:02 +0900

    :zap: Bookmark 데이터를 구하는  코드 줄임
    Card 컴포넌트 props에 bookmarkList 추가

--- src/routes/Bookmark.tsx
+++ src/routes/Bookmark.tsx
@@ -1,9 +1,9 @@
+import { useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, removeLocation, LocationType } from "../store";
 import { GetDustData } from "../utils/getDustData";
 import { SelctLocationData } from '../utils/utils';
 import Card from "../components/Card";
-import { useEffect } from "react";
 
 interface DataType extends SelctLocationData {
   sidoName: string
@@ -14,22 +14,29 @@ function Bookmark() {
   const bookmarkList = useSelector((state: RootState) => state.bookmark)
   console.log(bookmarkList)
 
-  const allDustData = GetDustData('전국');
   
-  let res: DataType[] = []
-
-  // useEffect(() => {
-  // }, [allDustData.data])
+  const allDustData = GetDustData('전국');
+  let res: DataType[] = [];
+  
   if (allDustData.data !== null && allDustData.data !== undefined && bookmarkList.length > 0) {
     // console.log(allDustData.data);
-    for (let i=0; i<bookmarkList.length; i++) {
-      for (let j=0; j<allDustData.data.length; j++) {
-        if (bookmarkList[i].sidoN === allDustData.data[j].sidoName && bookmarkList[i].stationN === allDustData.data[j].stationName) res.push(allDustData.data[j])
-      }
-    }
+    res = [];
+    bookmarkList.forEach((location: LocationType) => {
+      const found = allDustData.data.find((data: DataType) => data.sidoName === location.sidoN && data.stationName === location.stationN);
+      res.push(found);
+    })
+    // for (let i=0; i<bookmarkList.length; i++) {
+    //   console.log(bookmarkList[i]);
+    //   for (let j=0; j<allDustData.data.length; j++) {
+    //     // console.log(allDustData.data[j]);
+    //     if (bookmarkList[i].sidoN === allDustData.data[j].sidoName && bookmarkList[i].stationN === allDustData.data[j].stationName) {
+    //       res.push(allDustData.data[j]);
+    //       console.log(allDustData.data[j]);
+    //     }
+    //   }
+    // }
   }
   console.log(res);
-
 
   return (
     <div>
@@ -37,7 +44,7 @@ function Bookmark() {
       { allDustData.isLoading && '로딩중' }
       { allDustData.isError && 'Error!' }
       { allDustData && (res.length !== 0) &&
-        res.map((value: DataType, idx: number) => <Card key={idx} sido={value.sidoName} dustData={value}></Card>) }
+        res.map((value: DataType, idx: number) => <Card key={idx} sido={value.sidoName} dustData={value} bookmarkList={bookmarkList}></Card>) }
     </div>
   )
 }
