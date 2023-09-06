7842fa4cdea18c0696b6eae77d01faad256da8c0
lemony452 <gij9808@gmail.com>
2023-09-06 17:04:23 +0900

    :recycle: BoomarkList에 데이터 분기 처리 및 GetFavoriteDustData 쿼리 사용

--- src/routes/Bookmark.tsx
+++ src/routes/Bookmark.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, removeLocation, LocationType } from "../store";
-import { GetDustData } from "../utils/getDustData";
+import { GetDustData, GetFavoriteDustData } from "../utils/getDustData";
 import { SelctLocationData } from '../utils/utils';
 import Card from "../components/Card";
 
@@ -14,39 +14,55 @@ function Bookmark() {
   const bookmarkList = useSelector((state: RootState) => state.bookmark)
   console.log(bookmarkList)
 
-  
-  const allDustData = GetDustData('전국');
-  let res: DataType[] = [];
-  
-  if (allDustData.data !== null && allDustData.data !== undefined && bookmarkList.length > 0) {
-    // console.log(allDustData.data);
-    res = [];
-    bookmarkList.forEach((location: LocationType) => {
-      const found = allDustData.data.find((data: DataType) => data.sidoName === location.sidoN && data.stationName === location.stationN);
-      res.push(found);
-    })
-    // for (let i=0; i<bookmarkList.length; i++) {
-    //   console.log(bookmarkList[i]);
-    //   for (let j=0; j<allDustData.data.length; j++) {
-    //     // console.log(allDustData.data[j]);
-    //     if (bookmarkList[i].sidoN === allDustData.data[j].sidoName && bookmarkList[i].stationN === allDustData.data[j].stationName) {
-    //       res.push(allDustData.data[j]);
-    //       console.log(allDustData.data[j]);
-    //     }
-    //   }
-    // }
-  }
-  console.log(res);
+  // let allDustData = GetFavoriteDustData(bookmarkList);
 
-  return (
-    <div>
-      { bookmarkList.length === 0 && '즐겨찾는 지역을 추가해주세요!' }
-      { allDustData.isLoading && '로딩중' }
-      { allDustData.isError && 'Error!' }
-      { allDustData && (res.length !== 0) &&
-        res.map((value: DataType, idx: number) => <Card key={idx} sido={value.sidoName} dustData={value} bookmarkList={bookmarkList}></Card>) }
-    </div>
-  )
+  // --------------------------------------------------
+  
+  // const { isLoading, isError, data } = GetDustData('전국');
+  
+  if (bookmarkList.length === 0) return (<div>즐겨찾는 지역을 추가해주세요!</div>)
+  else {
+    let res: DataType[] = [];
+    const getData = GetFavoriteDustData(bookmarkList);
+    const isLoading  = getData.some((res) => res.isLoading);
+    const isError = getData.some((res) => res.isError);
+    console.log(getData);
+    if (!isLoading && !isError) {
+      res = getData.map((value) => value.data)
+    }
+
+    // if (data !== null && data !== undefined) {
+    //   console.log(data);
+    //   res = [];
+    //   bookmarkList.forEach((location: LocationType) => {
+    //     console.log(location);
+    //     const found = data.find((dustData: DataType) => {
+    //       // console.log(dustData);
+    //       // console.log(dustData.sidoName === location.sidoN);
+    //       // console.log(dustData.stationName === location.stationN);
+    //       return dustData.sidoName === location.sidoN && dustData.stationName === location.stationN
+    //     });
+    //     console.log(found);
+    //     res.push(found);
+    //   })
+    //   // for (let i=0; i<bookmarkList.length; i++) {
+    //   //   for (let j=0; j<data.length; j++) {
+    //   //     if (bookmarkList[i].sidoN === data[j].sidoName && bookmarkList[i].stationN === data[j].stationName) {
+    //   //       res.push(data[j]);
+    //   //     }
+    //   //   }
+    //   // }
+    // }
+    console.log(res);
+    return (
+      <div>
+        { isLoading && '로딩중' }
+        { isError && 'Error!' }
+        { res.length > 0 &&
+          res.map((value: DataType, idx: number) => <Card key={idx} sido={value.sidoName} dustData={value} bookmarkList={bookmarkList}></Card>) }
+      </div>
+    )
+  }
 }
 
 export default Bookmark
