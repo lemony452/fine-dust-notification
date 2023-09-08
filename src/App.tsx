3e32fff22803cd9764d6e3efc947b79868e54368
lemony452 <gij9808@gmail.com>
2023-09-08 23:33:23 +0900

    :lipstick: App 컴포넌트 중앙 배치하도록 css 추가 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { useState } from 'react';
 import Main from './routes/main';
-import NavigationTap from './components/NavigationTap';
-import { Routes, Route, useNavigate, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import All from './routes/All';
 import Bookmark from './routes/Bookmark';
 import Layout from './components/Layout';
 
 function App() {
-
-  const navigate = useNavigate();
 
   if (localStorage.getItem('sido') === null) {
     localStorage.setItem('sido', '서울');
@@ -22,13 +16,11 @@ function App() {
 
   let [sido, setSido] = useState<string>(localStorage.getItem('sido')!);
   let [station, setStation] = useState<string>(localStorage.getItem('station')!);
-  // let [totalData, setTotal] = useState<object[] | null>();
-  // let [myLocation, setMyLocation] = useState<MyLocation | null>();
 
   let [tabNum, setTabNum] = useState<number>(0);
 
   return (
-    <div className="App relative w-128 p-5 h-[50rem] bg-white rounded-xl">
+    <div className="m-auto relative w-128 p-5 h-[50rem] bg-white rounded-xl">
       <Routes>
         <Route path='/' element={<Layout tabNum={tabNum} setTabNum={setTabNum} />}>
           <Route index element={<Main sido={sido} station={station} setSido={setSido} setStaion={setStation} />}></Route>
