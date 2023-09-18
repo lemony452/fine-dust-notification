5eee98af1299ba93ee3efb4b8615bcd9d071aec6
lemony452 <gij9808@gmail.com>
2023-09-18 19:41:42 +0900

    :recycle: Main에 MainCard 컴포넌트 적용해 리팩토링
    grade, sido, myLocation 데이터 Props로 전달

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, Dispatch, SetStateAction} from 'react';
 import { SelctLocationData } from '../utils/utils';
 import { GetDustData, sidoList } from "../utils/getDustData"
-import { FaRegFaceLaughSquint, FaRegFaceSmile, FaRegFaceMeh, FaRegFaceFrown, FaRegFaceTired, FaQuestion } from 'react-icons/fa6';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
+import MainCard from '../components/MainCard';
 
 interface PropsType {
   sido: string,
@@ -25,6 +25,8 @@ function Main({ sido, station, setSido, setStaion }: PropsType) {
     pm25Value: 0,
   };
 
+  let grade = 0;
+
   // 선택한 시/도별 station 리스트 가져오기
   if (res.data !== undefined || null) {
     let stationList: string[] = [];
@@ -45,6 +47,7 @@ function Main({ sido, station, setSido, setStaion }: PropsType) {
       pm25Grade: Number(myValue.pm25Grade),
       pm25Value: Number(myValue.pm25Value),
     }
+    grade = Math.max(myLocation.pm10Grade!, myLocation.pm25Grade!);
   }
 
   const selectSido = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -94,124 +97,7 @@ function Main({ sido, station, setSido, setStaion }: PropsType) {
       { res.isError && <Error /> }
       { res.isSuccess &&
         <div className='flex justify-center relative h-full'>
-        
-          { myLocation.pm10Grade! === 0 && myLocation.pm25Grade! === 0 ?
-            <div className='flex items-center justify-center flex-col p-10 w-full rounded-lg border border-gray-200 bg-gray-400 shadow-md'>
-              <div className='absolute top-10 left-5'>
-                <span className='text-2xl font-bold pr-1 text-gray-800'>{ myLocation.stationName }</span>
-                <span className='font-bold text-gray-700'>{ sido }</span>
-              </div>
-              <FaQuestion className='' size={150} color='white' />
-              <p className='text-4xl font-bold text-white py-3'>알수없음</p>
-              <div className='absolute bottom-3 right-5 text-gray-700'>{ myLocation.dataTime }</div>
-            </div> : null
-          }
-          { Math.max(myLocation.pm10Grade!, myLocation.pm25Grade!) === 1 ?
-            <div className='flex items-center justify-center flex-col p-10 w-full rounded-lg border border-gray-200 bg-blue-400 shadow-md'>
-              <div className='absolute top-10 left-5'>
-                <span className='text-2xl font-bold pr-1 text-gray-800'>{ myLocation.stationName }</span>
-                <span className='font-bold text-gray-700'>{ sido }</span>
-              </div>
-              <FaRegFaceLaughSquint size={150} color='white' />
-              <p className='text-4xl font-bold text-white py-3'>좋음</p>
-              <div className='flex w-full justify-between px-12 mt-7'>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm10Value } ㎍/㎥</div>
-                </div>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>초미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm25Value } ㎍/㎥</div>
-                </div>
-              </div>
-              <div className='absolute bottom-3 right-5 text-gray-700'>{ myLocation.dataTime }</div>
-            </div> : null
-          }
-          { Math.max(myLocation.pm10Grade!, myLocation.pm25Grade!) === 2 ?
-              <div className='flex items-center justify-center flex-col p-10 w-full rounded-lg border border-gray-200 bg-emerald-400 shadow-md'>
-                <FaRegFaceSmile size={150} color='white' />
-              <div className='absolute top-10 left-5'>
-                <span className='text-2xl font-bold pr-1 text-gray-800'>{ myLocation.stationName }</span>
-                <span className='font-bold text-gray-700'>{ sido }</span>
-              </div>
-              <p className='text-4xl font-bold text-white py-3'>보통</p>
-              <div className='flex w-full justify-between px-12 mt-7'>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm10Value } ㎍/㎥</div>
-                </div>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>초미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm25Value } ㎍/㎥</div>
-                </div>
-              </div>
-              <div className='absolute bottom-3 right-5 text-gray-700'>{ myLocation.dataTime }</div>
-            </div> : null
-          }
-          { Math.max(myLocation.pm10Grade!, myLocation.pm25Grade!) === 3 ?
-              <div className='flex items-center justify-center flex-col p-10 w-full rounded-lg border border-gray-200 bg-yellow-400 shadow-md'>
-                <FaRegFaceMeh size={150} color='white' />
-              <div className='absolute top-10 left-5'>
-                <span className='text-2xl font-bold pr-1 text-gray-800'>{ myLocation.stationName }</span>
-                <span className='font-bold text-gray-700'>{ sido }</span>
-              </div>
-              <FaRegFaceLaughSquint size={150} color='white' />
-              <p className='text-4xl font-bold text-white py-3'>좋음</p>
-              <div className='flex w-full justify-between px-12 mt-7'>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm10Value } ㎍/㎥</div>
-                </div>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>초미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm25Value } ㎍/㎥</div>
-                </div>
-              </div>
-              <div className='absolute bottom-3 right-5 text-gray-700'>{ myLocation.dataTime }</div>
-            </div> : null
-          }
-          { Math.max(myLocation.pm10Grade!, myLocation.pm25Grade!) === 4 ?
-            <div className='flex items-center justify-center flex-col p-10 w-full rounded-lg border border-gray-200 bg-orange-400 shadow-md'>
-              <FaRegFaceFrown size={150} color='white' />
-              <div className='absolute top-10 left-5'>
-                <span className='text-2xl font-bold pr-1 text-gray-800'>{ myLocation.stationName }</span>
-                <span className='font-bold text-gray-700'>{ sido }</span>
-              </div>
-              <p className='text-4xl font-bold text-white py-3'>좋음</p>
-              <div className='flex w-full justify-between px-12 mt-7'>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm10Value } ㎍/㎥</div>
-                </div>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>초미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm25Value } ㎍/㎥</div>
-                </div>
-              </div>
-              <div className='absolute bottom-3 right-5 text-gray-700'>{ myLocation.dataTime }</div>
-            </div> : null
-          }
-          { Math.max(myLocation.pm10Grade!, myLocation.pm25Grade!) === 5 ?
-            <div className='flex items-center justify-center flex-col p-10 w-full rounded-lg border border-gray-200 bg-rose-400 shadow-md'>
-              <FaRegFaceTired size={150} color='white' />
-              <div className='absolute top-10 left-5'>
-                <span className='text-2xl font-bold pr-1 text-gray-800'>{ myLocation.stationName }</span>
-                <span className='font-bold text-gray-700'>{ sido }</span>
-              </div>
-              <p className='text-4xl font-bold text-white py-3'>좋음</p>
-              <div className='flex w-full justify-between px-12 mt-7'>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm10Value } ㎍/㎥</div>
-                </div>
-                <div className='block text-center'>
-                  <div className='text-xl font-bold text-gray-800'>초미세먼지</div>
-                  <div className='text-lg text-gray-700'>{ myLocation.pm25Value } ㎍/㎥</div>
-                </div>
-              </div>
-              <div className='absolute bottom-3 right-5 text-gray-700'>{ myLocation.dataTime }</div>
-            </div> : null
-          }
+          <MainCard grade={grade} sido={sido} myLocation={myLocation}  />
         </div>
       }
     </div>
