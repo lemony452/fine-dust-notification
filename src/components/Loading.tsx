2d781e3a39b0d4511413fcb5231f4483bc5167d1
lemony452 <gij9808@gmail.com>
2023-09-18 16:49:52 +0900

    :lipstick: Error&Loading 컴포넌트 css 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className='relative w-[26rem] h-[32rem] border-gray-200 bg-blue-200 shadow-md rounded p-3 mt-5 animate-pulse'>
+    <div className='relative w-full h-full border-gray-200 bg-blue-200 shadow-md rounded p-3 animate-pulse'>
       <div className='absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2
         flex flex-row items-center text-2xl gap-3 font-bold tracking-wide'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10 fill-blue-500">
